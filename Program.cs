ebbd5a76520b417a331713f89cb31a12467ce927
foxneZz <foxnezz@gmail.com>
2017-10-18 20:00:52 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EightQueens
+{
+    class Program
+    {
+        // ----------------------------------
+
+        const int N = 8;
+        const bool drawSolutions = false;
+
+        // ----------------------------------
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"N: {N}\nCalculating...");
+
+            var sw = new Stopwatch();
+            sw.Start();
+            var solutions = Solver.GetSolutions(N);
+            sw.Stop();
+            
+            if (drawSolutions)
+            {
+                foreach (var solution in solutions)
+                {
+                    PrintField(solution);
+                    Console.WriteLine();
+                }
+            }
+            
+            Console.WriteLine($"Solution count: {solutions.Count}\nTime elapsed: {sw.Elapsed}");
+            Console.ReadLine();
+        }
+
+        static void PrintField(bool[,] field)
+        {
+            for (int y = 0; y < field.GetLength(0); ++y)
+            {
+                for (int x = 0; x < field.GetLength(1); ++x)
+                {
+                    if (field[x, y])
+                    {
+                        const ConsoleColor c = ConsoleColor.Cyan;
+                        Console.ForegroundColor = c;
+                        Console.BackgroundColor = c;
+                        Console.Write('Q');
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                    else
+                    {
+                        Console.Write('O');
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Solver.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace EightQueens
+{
+    public static class Solver
+    {
+        private static int[,] upperNeighbors =
+        {
+            { -1, -1 },
+            { 0, -1 },
+            { 1, -1 }
+        };
+
+        public static List<bool[,]> GetSolutions(int N)
+        {
+            var field = new bool[N, N];
+            for (int y = 0; y < N; ++y)
+                for (int x = 0; x < N; ++x)
+                    field[x, y] = false;
+
+            var solutions = new List<bool[,]>();
+            Solve(N, field, 0, ref solutions);
+            return solutions;
+        }
+
+        private static void Solve(int N, bool[,] field, int currentRow, ref List<bool[,]> solutions)
+        {
+            if (currentRow == N)
+            {
+                var clone = new bool[N, N];
+                for (int y = 0; y < N; ++y)
+                    for (int x = 0; x < N; ++x)
+                        clone[x, y] = field[x, y];
+
+                solutions.Add(clone);
+                return;
+            }
+
+            for (int x = 0; x < N; ++x)
+            {
+                bool queenFits = true;
+
+                for (int i = 0; i < upperNeighbors.GetLength(0); ++i)
+                {
+                    int c = 1;
+                    while (true)
+                    {
+                        int nx = x + (upperNeighbors[i, 0] * c);
+                        int ny = currentRow + (upperNeighbors[i, 1] * c);
+
+                        if (nx >= 0 && nx < N && ny >= 0 && ny < N)
+                        {
+                            if (field[nx, ny])
+                            {
+                                queenFits = false;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                        ++c;
+                    }
+
+                    if (!queenFits)
+                        break;
+                }
+
+                if (!queenFits)
+                    continue;
+
+                field[x, currentRow] = true;
+                Solve(N, field, currentRow + 1, ref solutions);
+                field[x, currentRow] = false;
+            }
+        }
+    }
+}
